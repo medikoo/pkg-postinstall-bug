6a87b1eec87420620f5752ae59bf573dcb6f2a8d
Piotr Grzesik <pj.grzesik@gmail.com>
2022-04-07 13:31:47 +0200

    fix: Fix config

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ module.exports = {
         './node_modules/npm/bin/npm-cli.js',
         // Below module is not automatically traced by pkg, we need to point it manually
         // See: https://github.com/npm/npm-lifecycle/pull/41
-        '../../node_modules/node-gyp/bin/node-gyp.js',
-        '../../node_modules/npm/node_modules/node-gyp/bin/node-gyp.js',
-        '../../node_modules/npm/node_modules/npm-lifecycle/node_modules/node-gyp/bin/node-gyp.js',
+        './node_modules/node-gyp/bin/node-gyp.js',
+        './node_modules/npm/node_modules/node-gyp/bin/node-gyp.js',
+        './node_modules/npm/node_modules/npm-lifecycle/node_modules/node-gyp/bin/node-gyp.js',
     ]
 };

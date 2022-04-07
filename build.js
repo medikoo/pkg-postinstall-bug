58c3109ea0ea1f50d016573883be6cd299fc2f0e
Mariusz Nowak <medyk@medikoo.com>
2022-04-07 16:28:47 +0200

    Update

--- build.js
+++ build.js
@@ -1,31 +1,20 @@
 #!/usr/bin/env node
 
-// Node.js v8+ only
-
 "use strict";
 
-require("essentials");
+const childProcess = require("child_process");
 
-const path = require("path");
-const spawn = require("child-process-ext/spawn");
-const fse = require("fs-extra");
-
-const spawnOptions = { stdio: "inherit" };
-
-(async () => {
-  process.stdout.write("Build binaries\n");
-  await spawn(
-    "node",
-    [
-      "./node_modules/.bin/pkg",
-      "-c",
-      "config.js",
-      "--targets",
-      "node16-linux-x64,node16-mac-x64,node16-win-x64",
-      "--out-path",
-      "dist",
-      "index.js",
-    ],
-    spawnOptions
-  );
-})();
+childProcess.spawn(
+  "node",
+  [
+    "./node_modules/.bin/pkg",
+    "-c",
+    "config.js",
+    "--targets",
+    "node16-linux-x64,node16-mac-x64,node16-win-x64",
+    "--out-path",
+    "dist",
+    "index.js",
+  ],
+  { stdio: "inherit" }
+);
--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 "use strict";
 
-const spawn = require("child-process-ext/spawn");
+const childProcess = require("child_process");
 const path = require("path");
 const localNpmBinPath = path.join(
   __dirname,
   "./node_modules/npm/bin/npm-cli.js"
 );
 
-(async () => {
-  console.log("RUN", "node", [
-    localNpmBinPath,
-    "install",
-    "--no-save",
-    "es5-ext",
-  ]);
-  await spawn("node", [localNpmBinPath, "install", "--no-save", "es5-ext"], {
+console.log("RUN", "node", [
+  localNpmBinPath,
+  "install",
+  "--no-save",
+  "es5-ext",
+]);
+childProcess.spawn(
+  "node",
+  [localNpmBinPath, "install", "--no-save", "es5-ext"],
+  {
     stdio: "inherit",
-  });
-})();
+  }
+);

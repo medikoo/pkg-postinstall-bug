d4cf6623873d9c41ac6b47b0debb9b97538cafd3
Piotr Grzesik <pj.grzesik@gmail.com>
2022-04-07 13:34:56 +0200

    fix: Config

--- config.js
+++ config.js
@@ -3,12 +3,12 @@
 module.exports = {
     license: 'MIT',
     files: [
-        // Ensure npm is bundled as a dependency
-        './node_modules/npm/bin/npm-cli.js',
-        // Below module is not automatically traced by pkg, we need to point it manually
-        // See: https://github.com/npm/npm-lifecycle/pull/41
-        './node_modules/node-gyp/bin/node-gyp.js',
-        './node_modules/npm/node_modules/node-gyp/bin/node-gyp.js',
-        './node_modules/npm/node_modules/npm-lifecycle/node_modules/node-gyp/bin/node-gyp.js',
+      // Ensure npm is bundled as a dependency
+      './node_modules/npm/bin/npm-cli.js',
+      // Below module is not automatically traced by pkg, we need to point it manually
+      // See: https://github.com/npm/npm-lifecycle/pull/41
+      './node_modules/node-gyp/bin/node-gyp.js',
+      './node_modules/npm/node_modules/node-gyp/bin/node-gyp.js',
+      './node_modules/npm/node_modules/npm-lifecycle/node_modules/node-gyp/bin/node-gyp.js',
     ]
 };
